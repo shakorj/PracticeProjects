b7fb95bea7100224a64e8dc4d1935222bb044d88
shakorj <59297380+shakorj@users.noreply.github.com>
2019-12-27 19:50:17 -0600

    Delete WS1.py

--- WS1.py
+++
@@ -1,6 +0,0 @@
-#!C:\Users\Z003G8P\AppData\Local\Continuum\anaconda3\python.exe
-name = input("Give me your name: ")
-print("Your name is " + name)
-age = input("Enter your age: ")
-age = int(age)
-print("you will be 100 in " + str(2019 + 100 - age))
